b68159170c0f1fdbd30c70d453c2954aa316feaa
Richard Benson <richard.d.benson@gmail.com>
2012-10-04 22:23:16 -0500

    Assignment 3 works, but is ugly

---
+++ js/as3core.js
@@ -0,0 +1,57 @@
+/**
+ * @author Richard Benson
+ */
+function process(inputStack){
+	var workingStack = [];
+	var currentValue = 0;
+	var result = 0;
+	var operand = function(){};
+	
+	while (inputStack.length > 0){
+		currentValue = inputStack.pop();
+		if (currentValue.match(/^[\*\+\-\/]$/)) {
+			switch(String(currentValue))
+			{
+				case "+":
+					operand = pcadd;
+				break;
+				case "*":
+					operand = pcmultiply;
+				break;
+				case "/":
+					operand = pcdivide;
+				break;
+				case "-":
+					operand = pcsubtract;
+				break;
+			}
+			try	{
+				workingStack.push(operand(workingStack.pop(),workingStack.pop()));
+			}
+			catch(e)
+			{
+				alert(e.message);
+			}
+		} else {
+			workingStack.push(parseFloat(currentValue));
+		}
+	}
+	$("#result").text(workingStack.join(""));
+	return workingStack.join("");
+}
+
+function pcadd(a,b){
+		return "("+a+"+"+b+")";
+}
+
+function pcsubtract(a,b){
+		return "("+a+"-"+b+")";
+}
+function pcmultiply(a,b){
+		return "("+a+"*"+b+")";
+}
+function pcdivide (a,b){
+		return "("+a+"/"+b+")";
+}
+
+$("#pc-run").click(function(){process($("#pc-input").val().split(" ").reverse())});
